92e4f584fab3020e0611f8a256b5552b588d9253
Huang Grace <huang.yahui.grace@gmail.com>
2019-11-16 11:43:36 +0800

    新增view並璇轉

--- UIView/ViewController.swift
+++ UIView/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var myView: UIView!
+    //var theView:UIView!
+    var timer:Timer!
+    var counter = 0.0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        var newView:UIView!
+        newView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        newView.center = self.view.center
+        newView.backgroundColor = UIColor.red
+        newView.transform = CGAffineTransform(rotationAngle: 0)
+        newView.layer.cornerRadius = 15
+        newView.clipsToBounds = true
+        self.view.addSubview(newView)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: {
+            (timer) in
+            //self.rotateView(targetView: self.theView)
+            self.rotateView(targetView: newView)
+        })
     }
-
+    
+    func rotateView(targetView: UIView) {
+        let angle = counter * Double.pi / 180
+        targetView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        counter += 4
+        
+        //print(counter)
+    }
+    
     @IBAction func segmentAction(_ sender: UISegmentedControl) {
         print(sender.selectedSegmentIndex)
     }
